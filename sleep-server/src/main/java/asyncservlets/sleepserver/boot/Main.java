d2ce6af6660840f89ced744cef6c132e763c1747
Henrik Lundahl <henrik.lundahl@gmail.com>
2014-05-08 20:53:02 +0200

    Add server based on raw servlet.

---
+++ sleep-server/src/main/java/asyncservlets/sleepserver/boot/Main.java
@@ -0,0 +1,37 @@
+package asyncservlets.sleepserver.boot;
+
+import static org.eclipse.jetty.servlet.ServletContextHandler.SESSIONS;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+import asyncservlets.sleepserver.infra.rest.api.RootResourceServlet;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Server s = new Server(new QueuedThreadPool(2, 1));
+
+		s.setConnectors(new Connector[] { createConnector(s) });
+
+		ServletContextHandler context = new ServletContextHandler(SESSIONS);
+		context.setContextPath("/");
+		ServletHolder servletHolder = new ServletHolder(
+				new RootResourceServlet());
+		servletHolder.setInitOrder(1);
+		context.addServlet(servletHolder, "/*");
+		s.setHandler(context);
+
+		s.start();
+	}
+
+	private static ServerConnector createConnector(Server s) {
+		ServerConnector connector = new ServerConnector(s, 0, 1);
+		connector.setHost("localhost");
+		connector.setPort(8001);
+		return connector;
+	}
+}
---
+++ sleep-server/src/main/java/asyncservlets/sleepserver/infra/rest/api/RootResourceServlet.java
@@ -0,0 +1,59 @@
+package asyncservlets.sleepserver.infra.rest.api;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RootResourceServlet extends HttpServlet {
+	private static AtomicInteger pendingRequests = new AtomicInteger();
+
+	private Timer timer;
+
+	public RootResourceServlet() {
+		timer = new Timer();
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest req, HttpServletResponse resp) {
+		long replyAfterMillis = replyAfterMillisParam(req);
+		System.out.println(pendingRequests.incrementAndGet());
+		final AsyncContext context = req.startAsync();
+		context.setTimeout(120000);
+		final long before = System.currentTimeMillis();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println(pendingRequests.decrementAndGet());
+				long time = System.currentTimeMillis() - before;
+				ServletResponse response = context.getResponse();
+				response.setContentType("text/plain");
+				byte[] entity = ("Replying after " + time + " milliseconds.\n")
+						.getBytes();
+				response.setContentLength(entity.length);
+				try {
+					response.getOutputStream().write(entity);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				context.complete();
+			}
+		}, replyAfterMillis);
+	}
+
+	private long replyAfterMillisParam(HttpServletRequest req) {
+		String replyAfterMillisParam = req.getParameter("replyAfterMillis");
+		if (replyAfterMillisParam == null)
+			replyAfterMillisParam = "1000";
+		long replyAfterMillis = Long.parseLong(replyAfterMillisParam);
+		if (replyAfterMillis <= 0)
+			replyAfterMillis = 1000;
+		return replyAfterMillis;
+	}
+}
