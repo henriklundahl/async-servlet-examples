8b83a83b2452ac31561bd35ce8980b6332bae9c1
Henrik Lundahl <henrik.lundahl@gmail.com>
2014-05-08 20:57:22 +0200

    Add server based on Jersey.

---
+++ jersey-sleep-server/src/main/java/asyncservlets/jerseysleepserver/boot/Main.java
@@ -0,0 +1,45 @@
+package asyncservlets.jerseysleepserver.boot;
+
+import static org.eclipse.jetty.servlet.ServletContextHandler.SESSIONS;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+import asyncservlets.jerseysleepserver.infra.rest.api.RootResource;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Server s = new Server(new QueuedThreadPool(2, 1));
+
+		s.setConnectors(new Connector[] { createConnector(s) });
+
+		ServletContextHandler context = new ServletContextHandler(SESSIONS);
+		context.setContextPath("/");
+		ServletHolder servletHolder = new ServletHolder(createJerseyServlet());
+		servletHolder.setInitOrder(1);
+		context.addServlet(servletHolder, "/*");
+		s.setHandler(context);
+
+		s.start();
+	}
+
+	private static ServerConnector createConnector(Server s) {
+		ServerConnector connector = new ServerConnector(s, 0, 1);
+		connector.setHost("localhost");
+		connector.setPort(8001);
+		return connector;
+	}
+
+	private static ServletContainer createJerseyServlet() {
+		ResourceConfig resourceConfig = new ResourceConfig();
+		resourceConfig.register(new RootResource());
+		ServletContainer servletContainer = new ServletContainer(resourceConfig);
+		return servletContainer;
+	}
+}
---
+++ jersey-sleep-server/src/main/java/asyncservlets/jerseysleepserver/infra/rest/api/RootResource.java
@@ -0,0 +1,42 @@
+package asyncservlets.jerseysleepserver.infra.rest.api;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.core.MediaType;
+
+@Path("")
+public class RootResource {
+	private static AtomicInteger pendingRequests = new AtomicInteger();
+
+	private Timer timer;
+
+	public RootResource() {
+		timer = new Timer();
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public void root(@QueryParam("replyAfterMillis") long replyAfterMillis,
+			@Suspended final AsyncResponse response) throws Exception {
+		System.out.println(pendingRequests.incrementAndGet());
+		final long before = System.currentTimeMillis();
+		if (replyAfterMillis <= 0)
+			replyAfterMillis = 1000;
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				System.out.println(pendingRequests.decrementAndGet());
+				long time = System.currentTimeMillis() - before;
+				response.resume("Replying after " + time + " milliseconds.\n");
+			}
+		}, replyAfterMillis);
+	}
+}
